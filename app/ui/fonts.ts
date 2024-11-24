07574237c279d19cacc3226a67dd5265635bd227
Tam Le <tritam.le54@gmail.com>
2024-11-24 10:24:53 +0700

    chore: set up code

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['vietnamese'] })
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'] })
